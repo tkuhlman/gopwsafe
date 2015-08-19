3380b66113972deeb741431736d4f1065ee7c8ef
Tim Kuhlman <tim.kuhlman@hp.com>
2015-08-18 21:51:00 -0600

    Move to https://github.com/google/gxui/, it will be better long term

--- gui/gui.go
+++ gui/gui.go
@@ -1,54 +1,64 @@
 package gui
 
 import (
-	"github.com/tkuhlman/gopwsafe/pwsafe"
-
 	"github.com/andlabs/ui"
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/gxfont"
+	"github.com/google/gxui/math"
+	"github.com/google/gxui/themes/light"
 )
 
 var window ui.Window
 
-//Start Begins execution of the gui
-func Start(dbFile string) int {
-	// Consider using OpenFile more details at http://godoc.org/github.com/andlabs/ui
-	path := ui.NewTextField()
-	password := ui.NewTextField()
-	button := ui.NewButton("Open")
-	stack := ui.NewVerticalStack(
-		ui.NewLabel("Password file path:"),
-		path,
-		ui.NewLabel("Password:"),
-		password,
-		button)
-	window = ui.NewWindow("GoPWSafe", 200, 100, stack)
+func loginWindow(driver gxui.Driver) {
+	theme := light.CreateTheme(driver)
 
-	button.OnClicked(func() {
-		_, err := pwsafe.OpenPWSafeFile(path.Text(), password.Text())
-		if err == nil {
-			errorDialog("It worked")
-		} else {
-			errorDialog("Error opening file: " + err.Error())
-		}
-	})
-	window.OnClosing(func() bool {
-		ui.Stop()
-		return true
-	})
-	window.Show()
-
-	err := ui.Go()
+	font, err := driver.CreateFont(gxfont.Default, 25)
 	if err != nil {
 		panic(err)
 	}
-	return 0
+
+	window := theme.CreateWindow(380, 200, "GoPWSafe")
+
+	layout := theme.CreateLinearLayout()
+	layout.SetSizeMode(gxui.Fill)
+	layout.SetDirection(gxui.TopToBottom)
+
+	pathLabel := theme.CreateLabel()
+	pathLabel.SetFont(font)
+	pathLabel.SetText("Password DB path:")
+	layout.AddChild(pathLabel)
+
+	//todo add selectable entries from history
+	//todo add a file selector option
+	pathBox := theme.CreateTextBox()
+	//	pathBox.SetText(dbFile)
+	pathBox.SetPadding(math.ZeroSpacing)
+	pathBox.SetMargin(math.ZeroSpacing)
+	layout.AddChild(pathBox)
+
+	passwdLabel := theme.CreateLabel()
+	passwdLabel.SetFont(font)
+	passwdLabel.SetText("Password:")
+	layout.AddChild(passwdLabel)
+
+	passwordBox := theme.CreateTextBox()
+	passwordBox.SetPadding(math.ZeroSpacing)
+	passwordBox.SetMargin(math.ZeroSpacing)
+	layout.AddChild(passwordBox)
+
+	openButton := theme.CreateButton()
+	openButton.SetText("Open")
+	//	openButton.OnClick(func(gxui.MouseEvent) { action(); update() })
+	layout.AddChild(openButton)
+
+	window.AddChild(layout)
+	window.OnClose(driver.Terminate)
 }
 
-func errorDialog(msg string) {
-	stack := ui.NewVerticalStack(ui.NewLabel(msg))
-	window = ui.NewWindow("Error", 100, 100, stack)
-	window.OnClosing(func() bool {
-		ui.Stop()
-		return true
-	})
-	window.Show()
+//Start Begins execution of the gui
+func Start(dbFile string) int {
+	gl.StartDriver(loginWindow)
+	return 0
 }
